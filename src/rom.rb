13046ed614c883ae99f18496ba1922fc5b45f4d9
Yuji Yokoo <yujiyokoo@gmail.com>
2023-01-29 18:10:20 +0900

    Add get_long_word tests and get_byte (and tests)

--- src/rom.rb
+++ src/rom.rb
@@ -5,20 +5,21 @@ class Rom
     @contents = (contents)
   end
 
-  def at(addr)
+  def get_byte(addr)
+    raise BusError if contents[addr].nil?
     contents[addr]
   end
 
   # TODO: need a more efficient way of storing and getting bytes
   def get_word(addr)
     raise AddressError if addr.odd?
-    raise BusError if contents[addr].nil?
+    raise BusError if contents[addr..addr+1].size < 2
     (contents[addr] << 8) | ((contents[addr+1] & 0xFF))
   end
 
   def get_long_word(addr)
     raise AddressError if addr.odd?
-    raise BusError if contents[addr..addr+3].include?(nil?)
+    raise BusError if contents[addr..addr+3].size < 4
     ((contents[addr] & 0xFF) << 24) |
      ((contents[addr+1] & 0xFF) << 16) |
      ((contents[addr+2] & 0xFF) << 8) |
--- test/test_rom.rb
+++ test/test_rom.rb
@@ -10,10 +10,21 @@ describe Rom do
     end
   end
 
+  describe '#get_byte' do
+    it 'gets the byte from location' do
+      assert_equal 0x03, Rom.new([1, 2, 3, 4]).get_byte(2)
+    end
+
+    it 'raises a bus error if address points to an odd location' do
+      assert_raises(BusError) do
+        Rom.new([1, 2, 3, 4]).get_byte(4)
+      end
+    end
+  end
+
   describe '#get_word' do
     it 'gets the word from location' do
-      # 0x0304 is 772
-      assert_equal 772, Rom.new([1, 2, 3, 4]).get_word(2)
+      assert_equal 0x0304, Rom.new([1, 2, 3, 4]).get_word(2)
     end
 
     it 'raises an address error if address points to an odd location' do
@@ -25,6 +36,24 @@ describe Rom do
     it 'raises a bus error if address points to an odd location' do
       assert_raises(BusError) do
         Rom.new([1, 2, 3, 4]).get_word(4)
+      end
+    end
+  end
+
+  describe '#get_long_word' do
+    it 'gets the word from location' do
+      assert_equal 0x01020304, Rom.new([1, 2, 3, 4]).get_long_word(0)
+    end
+
+    it 'raises an address error if address points to an odd location' do
+      assert_raises(AddressError) do
+        Rom.new([1, 2, 3, 4]).get_long_word(1)
+      end
+    end
+
+    it 'raises a bus error if address points to an odd location' do
+      assert_raises(BusError) do
+        Rom.new([1, 2, 3, 4]).get_long_word(2)
       end
     end
   end
